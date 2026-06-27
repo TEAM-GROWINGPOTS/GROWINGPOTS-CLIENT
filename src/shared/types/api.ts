193d8961d7ae2088ee87c80f591dbaf752374fa8
mimizae <jim400934@gmail.com>
2026-06-27 15:02:24 +0900

    init: 공통 api 응답 타입 생성

---
+++ src/shared/types/api.ts
@@ -0,0 +1,23 @@
+type ApiResponseBase = {
+  code: string;
+  message: string;
+};
+
+export type SuccessResponse<T> = ApiResponseBase & {
+  success: true;
+  data: T;
+};
+
+export type EmptySuccessResponse = ApiResponseBase & {
+  success: true;
+  data: null;
+};
+
+export type ErrorResponse = ApiResponseBase & {
+  success: false;
+  data: null;
+};
+
+export type BaseResponse<T> = SuccessResponse<T> | EmptySuccessResponse;
+
+export type ApiResponse<T> = BaseResponse<T> | ErrorResponse;
