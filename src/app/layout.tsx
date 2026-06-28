7870f24130828c6042babacdfd53ad8fcf8214cc
pwc2002 <pwc2002@naver.com>
2026-06-29 02:20:45 +0900

    feat: pretendard 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { pretendard } from "@shared/styles/fonts";
 import "@shared/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.variable}>
       <body>{children}</body>
     </html>
   );
