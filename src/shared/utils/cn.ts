992faf6b5834e175a926034b3c1bd2af5fada9ca
pwc2002 <pwc2002@naver.com>
2026-06-29 23:06:47 +0900

    feat: cva 유틸 함수 추가

--- src/shared/utils/cn.ts
+++ src/shared/utils/cn.ts
@@ -1,4 +1,15 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const isTypographyToken = (value: string) =>
+  /^(display|title|body|caption)-/.test(value);
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [{ text: [isTypographyToken] }],
+    },
+  },
+});
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
