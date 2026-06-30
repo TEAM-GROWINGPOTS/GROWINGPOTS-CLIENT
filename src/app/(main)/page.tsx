14fcaced91ce36467cb8ab547a63a6bc2bca28c6
twossu <nadao202@naver.com>
2026-07-01 03:20:25 +0900

    feat: 아이콘 prop 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState } from 'react';
 import { TextField } from '@shared/components';
+import { useState } from 'react';
 
 export default function Page() {
   const [value, setValue] = useState('');
@@ -9,6 +9,7 @@ export default function Page() {
   return (
     <div className="p-4">
       <TextField placeholder="옹심아화이팅" value={value} onChange={setValue} maxLength={10} />
+      <TextField placeholder="옹심아화이팅" icon="ic_search" value={value} onChange={setValue} maxLength={10} />
     </div>
   );
 }
--- src/shared/components/text-field/text-field.tsx
+++ src/shared/components/text-field/text-field.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import { cn } from '@shared/utils/cn';
-import { InputHTMLAttributes, ChangeEvent } from 'react';
+import { ChangeEvent, InputHTMLAttributes } from 'react';
+
+import Icon from '../icon/icon';
 
 interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'maxLength'> {
   value: string;
   onChange: (value: string) => void;
   maxLength: number;
   placeholder: string;
+  icon?: string;
 }
 
-export const TextField = ({ value, onChange, maxLength, placeholder, ...props }: TextFieldProps) => {
+export const TextField = ({ value, onChange, maxLength, placeholder, icon, ...props }: TextFieldProps) => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length > maxLength) {
       e.target.value = e.target.value.slice(0, maxLength);
@@ -22,6 +25,7 @@ export const TextField = ({ value, onChange, maxLength, placeholder, ...props }:
     <div
       className={`flex h-12 items-center rounded-[10px] border border-gray-200 bg-white px-4 focus-within:border-blue-300`}
     >
+      {icon && <Icon name={icon} size={24} className="mr-3" />}
       <input
         value={value}
         className="text-body-r-16 min-w-0 flex-1 text-gray-800 outline-none placeholder:text-gray-300"
@@ -31,7 +35,7 @@ export const TextField = ({ value, onChange, maxLength, placeholder, ...props }:
         placeholder={placeholder}
         {...props}
       />
-      <span className="text-body-r-14 shrink-0 text-gray-300">
+      <span className="text-body-r-14 ml-2 shrink-0 text-gray-300">
         <span className={cn(value.length >= 1 && 'text-blue-500')}>{value.length}</span>/{maxLength}
       </span>
     </div>
