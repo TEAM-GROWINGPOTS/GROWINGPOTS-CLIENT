553d149b621a377332abb0de1b4fc4c752139a87
ssolsh <sohee220075@gmail.com>
2026-07-01 22:38:53 +0900

    feat: 카카오 로그인 버튼 UI 구현 (#42)

--- src/app/(auth)/login/kakao-login-button.tsx
+++ src/app/(auth)/login/kakao-login-button.tsx
@@ -2,17 +2,10 @@
 
 import Icon from '@shared/components/icon/icon';
 
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
-
 export const KakaoLoginButton = () => {
-  const handleClick = () => {
-    window.location.href = KAKAO_AUTH_URL;
-  };
-
   return (
     <button
       type="button"
-      onClick={handleClick}
       className="flex min-w-[280px] cursor-pointer items-center justify-between self-stretch rounded-[10px] border border-[#FEE500] bg-[#FEE500] px-4 py-3"
     >
       <Icon name="KakaoTalk" size={18} />
