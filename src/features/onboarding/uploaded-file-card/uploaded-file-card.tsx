76d0dc566277295be30392adde90dc56083f7c76
twossu <nadao202@naver.com>
2026-07-06 04:36:37 +0900

    fix: 파일 이름에 title 속성 추가

--- src/features/onboarding/uploaded-file-card/uploaded-file-card.tsx
+++ src/features/onboarding/uploaded-file-card/uploaded-file-card.tsx
@@ -21,7 +21,9 @@ export const UploadedFileCard = ({ fileName, fileSizeBytes, onRemove }: Uploaded
   <div className="flex h-69 items-center rounded-xl border border-gray-100 bg-gray-50 p-12">
     <div className="size-40 bg-amber-300" /> {/* TODO 아이콘이 들어올 것 만 같음. */}
     <div className="flex min-w-0 flex-1 flex-col pr-8 pl-12">
-      <span className="text-body-m-16 truncate text-gray-900">{fileName}</span>
+      <span title={fileName} className="text-body-m-16 truncate text-gray-900">
+        {fileName}
+      </span>
       <span className="text-body-r-14 text-gray-600">{formatFileSize(fileSizeBytes)}</span>
     </div>
     <button
