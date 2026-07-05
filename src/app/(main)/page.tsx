383758b836296f91e0ff1f1303ec8be3d9830439
twossu <nadao202@naver.com>
2026-07-05 23:20:58 +0900

    fix: 홈페이지 원복

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,14 +1,3 @@
-'use client';
-
-import { SearchField } from '@features/semester-planner/card-view/search-field/search-field';
-import { useState } from 'react';
-
 export default function Page() {
-  const [value, setValue] = useState('');
-
-  return (
-    <main>
-      <SearchField value={value} onChange={setValue} />
-    </main>
-  );
+  return <>home</>;
 }
