0900b38944ee87a9d89bb5e49c1eafd47d8a8c4d
twossu <nadao202@naver.com>
2026-07-06 02:59:41 +0900

    fix: 홈페이지 원복

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-
-import { TextField } from '@shared/components';
-import { useState } from 'react';
-
 export default function Page() {
-  const [value, setValue] = useState('');
-
-  return (
-    <div className="p-4">
-      <TextField placeholder="옹심아화이팅" value={value} onChange={setValue} maxLength={10} />
-      <TextField placeholder="옹심아화이팅" icon="ic_search" value={value} onChange={setValue} maxLength={10} />
-    </div>
-  );
+  return <>home</>;
 }
