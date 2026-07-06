35fbf6c14367f37bc81343f325a496978c85338c
ssolsh <sohee220075@gmail.com>
2026-07-07 00:15:06 +0900

    chore: 테스트 코드 제거

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,3 @@
-'use client';
-
-import { UploadedFileCard } from '@features/onboarding';
-import { useState } from 'react';
-
 export default function Page() {
-  const [file, setFile] = useState<File | null>(null);
-
-  return (
-    <div>
-      <input type="file" onChange={(e) => setFile(e.target.files?.[0] ?? null)} />
-      {file &&
-        (({ name, size }) => <UploadedFileCard fileName={name} fileSizeBytes={size} onRemove={() => setFile(null)} />)(
-          file,
-        )}
-    </div>
-  );
+  return <></>;
 }
