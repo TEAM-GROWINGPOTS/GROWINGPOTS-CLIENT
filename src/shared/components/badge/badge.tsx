35585ba0cd0069ec749d36d287d8b4f4900f8ab3
nohyewon <yw4913@naver.com>
2026-07-06 17:32:14 +0900

    refactor: Badge 디자인 변경사항 반영
    - Badge variant primary, secondary, disabled 기준으로 정리
    
    - xsmall 배지 우측 아이콘 옵션 추가

--- src/shared/components/badge/badge.tsx
+++ src/shared/components/badge/badge.tsx
@@ -1,8 +1,9 @@
+import Icon from '@shared/components/icon/icon';
 import { cn } from '@shared/utils/cn';
 import { cva, type VariantProps } from 'class-variance-authority';
 import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 
-const badgeVariants = cva('inline-flex w-fit items-center justify-center rounded-[0.25rem] whitespace-nowrap', {
+const badgeVariants = cva('inline-flex w-fit items-center justify-center gap-4 rounded-[0.25rem] whitespace-nowrap', {
   variants: {
     size: {
       xsmall: 'h-24 px-8 text-caption-m-12',
@@ -10,27 +11,71 @@ const badgeVariants = cva('inline-flex w-fit items-center justify-center rounded
       medium: 'h-40 px-20 text-body-sb-16',
     },
     variant: {
-      primary: 'bg-blue-100 text-blue-500',
-      secondary: 'bg-gray-100 text-gray-700',
-      outline: 'border border-gray-200 bg-white text-gray-700',
-      disabled: 'bg-gray-100 text-gray-400',
-      negative: 'bg-negative-20 text-negative-30',
+      primary: '',
+      secondary: 'bg-gray-600 text-gray-100',
+      disabled: 'bg-gray-100 text-gray-500',
+    },
+    color: {
+      lime01: '',
+      lime02: '',
+      purple: '',
+      blue: '',
+      red: '',
     },
   },
+  compoundVariants: [
+    {
+      variant: 'primary',
+      color: 'lime01',
+      className: 'bg-lime-200 text-lime-900',
+    },
+    {
+      variant: 'primary',
+      color: 'lime02',
+      className: 'bg-lime-100 text-lime-900',
+    },
+    {
+      variant: 'primary',
+      color: 'purple',
+      className: 'bg-purple-10 text-purple-20',
+    },
+    {
+      variant: 'primary',
+      color: 'blue',
+      className: 'bg-blue-10 text-blue-20',
+    },
+    {
+      variant: 'primary',
+      color: 'red',
+      className: 'bg-red-10 text-red-20',
+    },
+  ],
   defaultVariants: {
     size: 'medium',
     variant: 'primary',
+    color: 'lime01',
   },
 });
 
-interface BadgeProps extends Omit<ComponentPropsWithoutRef<'span'>, 'children'>, VariantProps<typeof badgeVariants> {
+interface BadgeProps
+  extends Omit<ComponentPropsWithoutRef<'span'>, 'children' | 'color'>, VariantProps<typeof badgeVariants> {
   children: ReactNode;
+  rightIconName?: string;
 }
 
-export const Badge = ({ children, className, variant = 'primary', size = 'medium', ...props }: BadgeProps) => {
+export const Badge = ({
+  children,
+  className,
+  variant = 'primary',
+  color = 'lime01',
+  size = 'medium',
+  rightIconName,
+  ...props
+}: BadgeProps) => {
   return (
-    <span className={cn(badgeVariants({ variant, size }), className)} {...props}>
+    <span className={cn(badgeVariants({ variant, color, size }), className)} {...props}>
       {children}
+      {size === 'xsmall' && rightIconName && <Icon name={rightIconName} size={16} />}
     </span>
   );
 };
