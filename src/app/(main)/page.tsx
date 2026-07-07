09e6ef3cd2cf5790c88be3e079a5fb0fc263e5bf
pwc2002 <pwc2002@naver.com>
2026-07-07 23:21:47 +0900

    refactor: 테스트를 위해 작성된 main page 내용 삭제

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,3 @@
-import { DropDown } from '@features/semester-planner/card-view';
-
 export default function Page() {
-  return (
-    <main className="flex h-dvh items-center justify-center gap-8 bg-gray-50">
-      <DropDown label="text" />
-      <DropDown label="text" status="selected" />
-    </main>
-  );
+  return <></>;
 }
