a8139490f0ad2d915e25b09a0d2da2d21e88ce52
haeni82 <2021111929@dgu.ac.kr>
2026-07-08 05:11:48 +0900

    feat: 졸업요건 충족 현황(노드뷰)아코디언 테스트코드 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,10 @@
+import { GraduationStatusAccordion } from '@features/semester-planner/road-view/graduation-status-accordion/graduation-status-accordion';
+
 export default function Page() {
-  return <></>;
+  return (
+    <main className="flex min-h-screen items-start justify-end gap-24 bg-white p-24">
+      <GraduationStatusAccordion hasDoubleMajor={false} />
+      <GraduationStatusAccordion hasDoubleMajor={true} />
+    </main>
+  );
 }
