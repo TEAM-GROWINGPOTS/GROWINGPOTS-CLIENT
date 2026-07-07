1b5efec5de2f36cc1ef1180c097b37ff3f702014
nohyewon <yw4913@naver.com>
2026-07-07 22:29:05 +0900

    feat: ConfirmModal 배럴 export 추가

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,6 +1,7 @@
 export { Badge } from './badge/badge';
 export type { ChipMode } from './chip/chip';
 export { Chip } from './chip/chip';
+export { ConfirmModal } from './modal/confirm-modal';
 export { Modal } from './modal/modal';
 export { Select } from './select/select';
 export { TextField } from './text-field/text-field';
