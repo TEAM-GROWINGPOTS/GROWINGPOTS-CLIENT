fc5de0b9cdae24d4adfa0fa970ce7368495c7377
haeni <149465566+haeni82@users.noreply.github.com>
2026-07-08 22:05:07 +0900

    Feat: chip 구조 변경 (#86)
    * feat: chip 구조 변경
    
    * feat: 코드리뷰 반영

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,5 +1,4 @@
 export { Badge } from './badge/badge';
-export type { ChipMode } from './chip/chip';
 export { Chip } from './chip/chip';
 export { ClassCard } from './class-card/class-card';
 export type { IconButtonSize } from './icon-button/icon-button';
