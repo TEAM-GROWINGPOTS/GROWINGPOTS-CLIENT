3019b6157d91b977f718b2b128cf3fc40d87e0bd
pwc2002 <40570332+pwc2002@users.noreply.github.com>
2026-07-10 14:00:34 +0900

    Feat: course-filter-modal 컴포넌트 구현 (#94)
    * feat: course-filter-modal 컴포넌트 구현
    
    * refactor: type 별칭 네이밍 규칙에 맞추어 변경
    
    * style: 변경된 디자인 가이드에 따라  고정 높이 설정
    
    * Update src/features/semester-planner/card-view/course-filter-modal/course-filter-modal.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix: tab 참조 에러 재수정
    
    * feat: 기타 필수 탭 추가
    
    * fix: 코드리뷰 반영 - 모달 재오픈시 상태 초기화
    
    * feat: 모달 기본 상태 prop 추가
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/features/semester-planner/card-view/index.ts
+++ src/features/semester-planner/card-view/index.ts
@@ -1,3 +1,5 @@
+export type { CourseFilterTabKeyTypes, CourseFilterValues } from './course-filter-modal/course-filter-modal';
+export { CourseFilterModal } from './course-filter-modal/course-filter-modal';
 export { DropDown } from './drop-down/drop-down';
 export { FolderItemMenu } from './folder-item-menu/folder-item-menu';
 export { FolderList } from './folder-list/folder-list';
