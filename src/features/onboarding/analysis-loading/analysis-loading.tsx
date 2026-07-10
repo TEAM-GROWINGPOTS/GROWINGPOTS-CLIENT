03e36d79fbe57cefd89389232254bb842226aa20
mimizae <jim400934@gmail.com>
2026-07-11 00:58:06 +0900

    refactor: 프로젝트 규칙에 맞춰 컴포넌트 루트에 Fragment 추가

--- src/features/onboarding/analysis-loading/analysis-loading.tsx
+++ src/features/onboarding/analysis-loading/analysis-loading.tsx
@@ -6,9 +6,11 @@ import animationData from './analyze.json';
 
 export const AnalysisLoading = () => {
   return (
-    <section className="z-analysis-loading fixed inset-0 flex flex-col items-center justify-center gap-12 bg-black/60">
-      <Lottie animationData={animationData} loop autoplay className="h-150 w-150" />
-      <p className="text-display-sb-28 text-white">졸업 현황을 분석하고 있어요</p>
-    </section>
+    <>
+      <section className="z-analysis-loading fixed inset-0 flex flex-col items-center justify-center gap-12 bg-black/60">
+        <Lottie animationData={animationData} loop autoplay className="h-150 w-150" />
+        <p className="text-display-sb-28 text-white">졸업 현황을 분석하고 있어요</p>
+      </section>
+    </>
   );
 };
