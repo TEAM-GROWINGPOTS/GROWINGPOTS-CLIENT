288b00a3a1ef073ab81b66730165db801dc6b6dd
ssolsh <sohee220075@gmail.com>
2026-07-11 04:25:22 +0900

    chore: 브라우저 확인용 테스트 코드 제거

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,3 @@
-import { LoginCarousel } from '@features/auth/login/login-carousel';
-
 export default function Page() {
-  return (
-    <div className="flex h-screen gap-32 bg-white p-32" style={{ zoom: 0.6 }}>
-      <div className="flex-[3]">
-        <LoginCarousel />
-      </div>
-      <div className="flex-[2]" />
-    </div>
-  );
+  return <></>;
 }
