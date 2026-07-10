5ed82ef0a735aba567b0d15515efafe50bed55e2
twossu <nadao202@naver.com>
2026-07-11 04:25:40 +0900

    fix: 졸업 요건 카드 관련 API 및 타입 삭제

--- src/features/onboarding/index.ts
+++ src/features/onboarding/index.ts
@@ -1,4 +1,3 @@
-export { toRequirementCards } from './api/to-requirement-cards';
 export { GraduationResult } from './graduation-result/graduation-result';
 export { PdfUploader } from './pdf-uploader';
 export { toast, Toaster } from './toast';
