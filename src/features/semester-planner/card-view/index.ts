4aadbc77aeefdad2d14c31c5a91fbe6e2d85be20
pwc2002 <40570332+pwc2002@users.noreply.github.com>
2026-07-10 21:12:41 +0900

    Feat: 과목추가 우측 패널 컴포넌트 구현 (#101)
    * hotfix: drop-down border 로 인한 사이즈 차이 수정
    
    * feat: noresult png 추가
    
    * feat: add-course-sidebar 컴포넌트 구현
    
    * feat: 직접추가 버튼 추가
    
    * Update src/features/semester-planner/card-view/add-course-sidebar/add-course-sidebar.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix: coderabbit  suggest 편집 에러 수정
    
    * style: 누락된 border-l 추가
    
    * refactor: 스크롤바 안보이게 속성 추가
    
    * fix: onDirectAdd 필수 prop 으로 변경
    
    * fix: 과목 추가 텍스트 띄어쓰기 수정
    
    * feat: 좌우 스크롤 버튼 및 효과 추가
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/features/semester-planner/card-view/index.ts
+++ src/features/semester-planner/card-view/index.ts
@@ -1,3 +1,4 @@
+export { AddCourseSidebar } from './add-course-sidebar/add-course-sidebar';
 export type { CourseFilterTabKeyTypes, CourseFilterValues } from './course-filter-modal/course-filter-modal';
 export { CourseFilterModal } from './course-filter-modal/course-filter-modal';
 export { DropDown } from './drop-down/drop-down';
