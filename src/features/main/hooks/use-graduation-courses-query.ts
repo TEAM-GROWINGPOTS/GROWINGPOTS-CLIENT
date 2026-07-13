3af0a35d20112f27c5661fca73374d4dc5875965
nohyewon <yw4913@naver.com>
2026-07-14 05:33:05 +0900

    refactor: useQueries combine 리렌더링 최적화

--- src/features/main/hooks/use-graduation-courses-query.ts
+++ src/features/main/hooks/use-graduation-courses-query.ts
@@ -5,16 +5,15 @@ import { QUERY_KEY } from '@shared/apis/query-key';
 import { useQueries } from '@tanstack/react-query';
 
 export const useGraduationCoursesQueries = (divisionCodes: RequirementCode[], params: GetGraduationCoursesParams) => {
-  const queries = useQueries({
+  return useQueries({
     queries: divisionCodes.map((divisionCode) => ({
       queryKey: QUERY_KEY.GRADUATION.COURSES(divisionCode, params),
       queryFn: () => getGraduationCourses({ divisionCode, ...params }),
     })),
+    combine: (queries) => ({
+      data: queries.flatMap(({ data }) => (data ? [data] : [])),
+      isPending: queries.some(({ isPending }) => isPending),
+      isError: queries.some(({ isError }) => isError),
+    }),
   });
-
-  return {
-    data: queries.flatMap(({ data }) => (data ? [data] : [])),
-    isPending: queries.some(({ isPending }) => isPending),
-    isError: queries.some(({ isError }) => isError),
-  };
 };
