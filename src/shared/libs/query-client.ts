83682055db4d00e19fcd79ffe295d2002ab661bb
mimizae <jim400934@gmail.com>
2026-07-14 00:28:38 +0900

    refactor: 전역 에러 토스트 메세지 설정 추가

--- src/shared/libs/query-client.ts
+++ src/shared/libs/query-client.ts
@@ -1,7 +1,18 @@
-import { QueryClient } from '@tanstack/react-query';
+import { toast } from '@shared/components/toast';
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
 
 export const createQueryClient = () =>
   new QueryClient({
+    queryCache: new QueryCache({
+      onError: () => {
+        toast.negative('요청에 실패했어요.');
+      },
+    }),
+    mutationCache: new MutationCache({
+      onError: () => {
+        toast.negative('요청에 실패했어요.');
+      },
+    }),
     defaultOptions: {
       queries: {
         staleTime: 1000 * 60 * 5,
