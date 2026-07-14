f4548312301c88d125e6dde45a51733828b0c14a
twossu <nadao202@naver.com>
2026-07-15 01:15:54 +0900

    fix: 재학 -> 재학 에 중 붙이기

--- src/features/onboarding/analysis-result/student-info/student-info.tsx
+++ src/features/onboarding/analysis-result/student-info/student-info.tsx
@@ -33,7 +33,7 @@ export const StudentInfo = ({
           <div className="flex items-center gap-10">
             <span className="text-title-sb-20 text-gray-700">{name}</span>
             <Badge size="xsmall" color="lime01">
-              {enrollmentStatus}
+              {enrollmentStatus} 중
             </Badge>
           </div>
           <span className="text-body-m-14 text-gray-400">{schoolName}</span>
